4978690d07f35c9b52850a73558005b7c34a70cd
DanielDeus <danielsilvadeus@gmail.com>
2023-02-15 23:40:01 -0300

    cap4 exercises and exemples

---
+++ cap4/4-2.py
@@ -0,0 +1,17 @@
+# 4-2. Animals: Think of at least three different animals that have a common characteristic. 
+# Store the names of these animals in a list, and then use a for loop to 
+# print out the name of each animal.
+
+# •	 Modify your program to print a statement about each animal, such as 
+# A dog would make a great pet.
+# •	 Add a line at the end of your program stating what these animals have in 
+# common. You could print a sentence such as Any of these animals would 
+# make a great pet!
+
+animals = ['cachorro', 'gato', 'leao', 'vaga']
+
+for a in animals:
+    print(f'A {a} would make a great pet!')
+
+
+print('Todos eles sao mamiferos e tem quatro patas.')
---
+++ cap4/magicians.py
@@ -0,0 +1,4 @@
+magicians = ['alice', 'david', 'carolina'] 
+
+for m in magicians:
+    print(f'{m.title()}, that was a great trick!')
